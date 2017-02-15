81934be67acfa148ffb2c4052e49f4b950ec69cf
dmazzer <dmazzer@gmail.com>
2017-02-15 06:54:57 -0200

    Dockerfiles cleanup, fix entrypoint files and docker-compose

--- mongodb/set_mongodb_password.sh
+++ mongodb/set_mongodb_password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER=${MONGODB_USER:-"admin"}
-DATABASE=${MONGODB_DATABASE:-"admin"}
+DATABASE=${MONGODB_DATABASE:-"nors_devices"}
 PASS=${MONGODB_PASS:-"admin"}
 #PASS=${MONGODB_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
@@ -15,7 +15,7 @@ while [[ RET -ne 0 ]]; do
 done
 
 echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
-mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'userAdminAnyDatabase',db:'admin'}]});"
 
 if [ "$DATABASE" != "admin" ]; then
     echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
--- server/run.sh
+++ server/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -xem
-
-# fix vi compatibility mode (for developers)
-echo "set nocompatible" > $HOME/.exrc
+set -em
 
 # repo sync
 cd $NORS_PATH
-git pull origin $NORS_BRANCH
+#git pull origin $NORS_BRANCH
+
+export NORS_MONGODB_PORT_27017_TCP_ADDR=mongodb
+export NORS_MONGODB_PORT_27017_TCP_PORT=27017
 
 # run nors server
+echo "=> Starting NORS server"
 nors_cmd="python3 nors_srv.py -c nors.conf"
 
 $nors_cmd &
